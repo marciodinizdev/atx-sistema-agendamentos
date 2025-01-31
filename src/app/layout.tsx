da8827caa34cf40f8e12e21eb6b0abf8943b9f2d
Márcio Diniz <marciodiniz.dev@gmail.com>
2025-01-31 15:48:28 -0300

    Updates

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,14 @@
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import "./styles/globals.css"
+
+export const metadata = {
+    title: "ATX Tech | Faça seu agendamento",
+    description: "Serviços especializados em suporte técnico",
+    icons: {
+        icon: "/favicon.png",
+    },
+}
 export default function Layout({ children }: { children: React.ReactNode }) {
     return (
         <html lang="pt">
