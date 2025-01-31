7c192c0ea1df79fa4ebcb774a4288da792fb1b03
Márcio Diniz <marciodiniz.dev@gmail.com>
2025-01-31 16:29:01 -0300

    Updates

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,11 +1,13 @@
 export default function Header() {
     return (
         <header className="header">
-            <a href="/"><img className="logo" src="logo.png" alt="" /></a>
+            <a href="/"><img className="logo" src="/logo.png" alt="Logo ATX Tech" /></a>
             <nav>
                 <ul>
                     <li><a href="/">Início</a></li>
                     <li><a href="/suporte">Suporte</a></li>
+                    <li><a href="/informacoes/sobre">Sobre</a></li>
+                    <li><a href="/informacoes/contato">Contato</a></li>
                 </ul>
             </nav>
         </header>
---
+++ src/app/informacoes/sobre/page.tsx
@@ -0,0 +1,8 @@
+export default function Sobre() {
+    return (
+        <div>
+            <h1>Sobre a ATX Tech</h1>
+            <p>Informações sobre a nossa empresa e serviços.</p>
+        </div>
+    );
+}
