fe476f65b6d9a8b20f39671e9a897fb83f6e586d
Márcio Diniz <marciodiniz.dev@gmail.com>
2025-01-31 19:26:03 -0300

    Updates

--- src/app/informacoes/sobre/page.tsx
+++ src/app/informacoes/sobre/page.tsx
@@ -2,7 +2,18 @@ export default function Sobre() {
     return (
         <div>
             <h1>Sobre a ATX Tech</h1>
-            <p>Informações sobre a nossa empresa e serviços.</p>
+            <p>A <strong>ATX Tech</strong> é uma empresa inovadora no setor de tecnologia, especializada em oferecer soluções completas para o desenvolvimento e manutenção de sistemas personalizados, além de realizar análises detalhadas de requisitos para projetos de negócios. Nosso objetivo é fornecer serviços de alta qualidade que atendam às necessidades específicas de cada cliente, ajudando-os a otimizar processos e alcançar seus objetivos de forma mais eficiente.</p><br/>
+
+            <p><strong>Nossos Serviços:</strong></p>
+            <ul>
+                <li><strong>Desenvolvimento de Sistemas:</strong> Criamos soluções sob medida, com foco na funcionalidade e escalabilidade, atendendo às demandas exclusivas de cada negócio.</li>
+                <li><strong>Manutenção de Sistemas:</strong> Oferecemos suporte contínuo para garantir que seus sistemas estejam sempre atualizados e operando de maneira eficiente.</li>
+                <li><strong>Análise de Requisitos:</strong> Realizamos uma análise detalhada das necessidades do seu negócio, transformando-as em projetos tecnológicos precisos e bem definidos, garantindo que os recursos sejam aplicados da forma mais eficaz possível.</li>
+            </ul><br />
+            <p><strong>Agendamento de Serviços</strong></p> 
+            <p>Na ATX Tech, você pode agendar nossos serviços de forma prática e eficiente, com flexibilidade para atender à sua agenda. Nosso objetivo é proporcionar a melhor experiência ao cliente, com transparência e compromisso com os prazos.</p>
+            <p>Com a ATX Tech, você pode confiar que está escolhendo uma parceira estratégica para o sucesso do seu projeto.</p>
+            
         </div>
     );
 }
