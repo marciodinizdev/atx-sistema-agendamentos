b67109a939c9a3cb364e47e7c8429fd881529cda
Márcio Diniz <marciodiniz.dev@gmail.com>
2025-01-30 22:25:11 -0300

    Updates

---
+++ src/app/page.tsx
@@ -0,0 +1,8 @@
+export default function HomePage() {
+    return (
+        <div>
+            <h1>Bem-vindo ao ATX Tech</h1>
+            <p>Faça o seu agendamento hoje mesmo e iremos resolver o seu problema!</p>
+        </div>
+    )
+}
---
+++ src/app/suporte/page.tsx
@@ -0,0 +1,8 @@
+export default function SuportePage() {
+    return (
+        <div>
+            <h1>Página de Suporte</h1>
+            <p>Aqui você encontra ajuda para seus problemas de informática.</p>
+        </div>
+    );
+}
