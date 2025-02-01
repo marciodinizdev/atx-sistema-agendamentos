2bb0c2fd74895bc0b7615b6549cf6ed69e0d378f
Márcio Diniz <marciodiniz.dev@gmail.com>
2025-02-01 16:59:37 -0300

    Updates

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,19 +1,56 @@
+"use client";
+
+import { usePathname } from "next/navigation";
 import Link from "next/link";
 import Image from "next/image";
 
 export default function Header() {
+    const pathName = usePathname();
+
     return (
         <header className="header">
             <nav>
                 <div className="nav-container">
                     <Link href="/">
-                    <Image className="logo" src="/logo.png" alt="Logo ATX Tech" width={0} height={0} sizes="140px" style={{ width: "auto", height: "auto" }}/>
+                        <Image 
+                            className="logo" 
+                            src="/logo.png" 
+                            alt="Logo ATX Tech" 
+                            width={0} 
+                            height={0} 
+                            sizes="140px" 
+                            style={{ width: "auto", height: "auto" }} 
+                        />
                     </Link>
                     <ul>
-                        <li><a href="/">Início</a></li>
-                        <li><a href="/suporte">Suporte</a></li>
-                        <li><a href="/informacoes/sobre">Sobre</a></li>
-                        <li><a href="/informacoes/contato">Contato</a></li>
+                        <li>
+                            <Link 
+                                href="/" 
+                                className={pathName === "/" ? "active" : ""}>
+                                Início
+                            </Link>
+                        </li>
+                        <li>
+                            <Link 
+                                href="/suporte" 
+                                className={pathName === "/suporte" ? "active" : ""}>
+                                Suporte
+                            </Link>
+                        </li>
+                        <li>
+                            <Link 
+                                href="/informacoes/sobre" 
+                                className={pathName === "/informacoes/sobre" ? "active" : ""}>
+                                Sobre
+                            </Link>
+                        </li>
+                        <li>
+                            <Link 
+                                href="/informacoes/contato" 
+                                className={pathName === "/informacoes/contato" ? "active" : ""}>
+                                Contato
+                            </Link>
+                        </li>
                     </ul>
                 </div>
             </nav>
