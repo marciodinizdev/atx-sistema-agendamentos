32ff388440ed0e4f0e206fb9123544131372e31b
Márcio Diniz <marciodiniz.dev@gmail.com>
2025-02-01 18:57:59 -0300

    Updates

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,6 +1,21 @@
+import Image from "next/image";
+import Link from "next/link";
+
 export default function Footer() {
     return (
         <footer className="footer">
+            <Link href="/">
+                <Image
+                    className="logo"
+                    src="/favicon.png"
+                    alt="Logo ATX Tech"
+                    width={0}
+                    height={0}
+                    sizes="50px"
+                    style={{ width: "auto", height: "auto" }}
+                />
+            </Link>
+
             <p>&copy; {new Date().getFullYear()} ATX Tech - Todos os direitos reservados.</p>
         </footer>
     );
