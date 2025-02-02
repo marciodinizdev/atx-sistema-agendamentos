adf5695f5fc15e5021e5fd6449aac6fed3d92c97
Márcio Diniz <marciodiniz.dev@gmail.com>
2025-02-02 19:58:02 -0300

    Create page.tsx

---
+++ src/app/agendamento/page.tsx
@@ -0,0 +1,9 @@
+export default function AgendamentoPage() {
+    return (
+        <div>
+            <h1>Página de Agendamento</h1>
+            <p>Agende a sua reunião reunião remota ou visita técnica.</p>
+            <p>Selecione o tipo de agendamento:</p>
+        </div>
+    );
+}
