c52ec87a7b0743b843df9c6e7c0667d0a7bb7f66
Márcio Diniz <marciodiniz.dev@gmail.com>
2025-02-06 20:50:06 -0300

    updates

--- src/app/informacoes/contato/page.tsx
+++ src/app/informacoes/contato/page.tsx
@@ -2,7 +2,34 @@ export default function Contato() {
     return (
         <div>
             <h1>Contato</h1>
-            <p>Entre em contato conosco pelo formulário abaixo.</p>
+            <p>Entre em contato conosco preenchendo o formulário abaixo ou utilizando um dos nossos canais de atendimento.</p>
+            
+            <h2>Informações de Contato</h2>
+            <p><strong>Email:</strong> suporte@atxtech.com</p>
+            <p><strong>Telefone:</strong> (11) 99999-9999</p>
+            <p><strong>Endereço:</strong> Rua Prefeito João Inácio, S/N - Barra de Santa Rosa, PB</p>
+            
+            <section className="contato-form">
+                <h2 >Formulário de Contato</h2>
+                <form>
+                    <label>
+                        <span>Nome:</span>
+                        <input type="text" name="nome" required />
+                    </label>
+                    <br />
+                    <label>
+                        <span>Email:</span>
+                        <input type="email" name="email" required />
+                    </label>
+                    <br />
+                    <label>
+                        <span>Mensagem:</span>
+                        <textarea name="mensagem" required></textarea>
+                    </label>
+                    <br />
+                    <button type="submit">Enviar</button>
+                </form>
+            </section>
         </div>
     );
 }
